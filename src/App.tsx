09f177dce00985cd86577f6be0cc6204a8db7916
codinci <dkibau@gmail.com>
2023-05-01 23:10:04 +0300

    designing dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { useMemo } from "react"
 import { themeSettings } from "./theme"
 import { Box, CssBaseline } from "@mui/material"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Navbar from "@/views/navbar"
+import Dashboard from "@/views/dashboard"
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), [])
@@ -14,8 +16,9 @@ function App() {
           <ThemeProvider theme={theme}>
             <CssBaseline />
             <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+              <Navbar/>
               <Routes>
-                <Route path="/" element={<div>Dashboard page</div>}/>
+                <Route path="/" element={ <Dashboard/> }/>
                 <Route path="/predictions" element={<div>Predictions page</div>}/>
               </Routes>
             </Box>
